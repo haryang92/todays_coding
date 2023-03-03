79607fde6e3c94c91f7a63e1edc9b3d5cf46dfbc
haryang Park <haryang92@gmail.com>
2023-03-03 10:05:20 +0900

    Initial Commit

---
+++ src/main/java/studio/thinkground/SpringTestProject/controller/PostController.java
@@ -0,0 +1,34 @@
+package studio.thinkground.SpringTestProject.controller;
+
+import org.springframework.web.bind.annotation.*;
+import studio.thinkground.testproject.dto.MemberDTO;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/post-api")
+public class PostController {
+
+    //    http://localhost:8080/api/v1/post-api/default
+    @PostMapping(value = "/default")
+    public String postMethod() {
+        return "Hello World!";
+    }
+    //    http://localhost:8080/api/v1/pist-api/member
+    @PostMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach(map-> {
+            sb.append(map.getKey() + ":" + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @PostMapping(value = "/member2")
+    public String postMemberDto(@RequestBody MemberDTO memberDTO) {
+        return memberDTO.toString();
+    }
+}
+
